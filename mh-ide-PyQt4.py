f82020f414ee827843e6235a01f1a3453b9405b0
ArunJayan <arunjayan32@gmail.com>
2015-02-09 21:20:09 +0530

    modifiing save()

--- mh-ide-PyQt4.py
+++ mh-ide-PyQt4.py
@@ -10,27 +10,44 @@ class Microhope_Main(QtGui.QMainWindow):
 		QtGui.QMainWindow.__init__(self,parent)
 		
 		self.app = app 
+		self.filename = ""
 		self.setFixedSize(1250,710) #here we fix the size to 1250x710 
 									#and prevent the resizing the window , and disable the maximising and minimising . 
 	def initToolbar(self):
 		# New Action
-		self.new = QtGui.QAction(QtGui.QIcon("icons/document-new.png"),"New",self) #for creating new files  . 
-		self.new.setShortcut("Ctrl+N")
-		self.new.setStatusTip("Create a new document.")
-		self.new.triggered.connect(self.new)
+		self.new_file = QtGui.QAction(QtGui.QIcon("icons/document-new.png"),"New",self) #for creating new files  . 
+		self.new_file.setShortcut("Ctrl+N")
+		self.new_file.setStatusTip("Create a new document.")
+		self.new_file.triggered.connect(self.new)
 		##             ##           ## 
 		#Open Action
 		self.open_file = QtGui.QAction(QtGui.QIcon("icons/document-open.png"),"Open file",self)
 		self.open_file.setStatusTip("Open existing document")
 		self.open_file.setShortcut("Ctrl+O")
 		self.open_file.self.openAction.triggered.connect(self.open)
+		##             ##           ##
+		# Save Action
+		self.save_file = self.saveAction = QtGui.QAction(QtGui.QIcon("icons/save.png"),"Save",self)
+		self.save_file.setStatusTip("Save document")
+		self.save_file.setShortcut("Ctrl+S")
+		self.save_file.triggered.connect(self.save)
+		##             ##           ##
+		#
 	# function for new document .
 	def new(self):
 		new = Microhope_Main(self.app)
 		new.show()
 	def open(self):
-		
-
+		self.filename = QtGui.QFileDialog.getOpenFileName(self, 'Open File',".","(*.*)")
+		if self.filename:
+			with open(self.filename,"rt",encoding="utf-8") as file:
+				self.text.setText(file.read())
+	def save(self):
+		if not self.filename:
+			self.filename = QtGui.QFileDialog.getSaveFileName(self, 'Save File')
+	
+	
+	
 
 if __name__ == "__main__":
 	
